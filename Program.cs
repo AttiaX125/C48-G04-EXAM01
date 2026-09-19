22da954c950d8b50b660da0265923fc8272faa94
AttiaX125 <AttiaX125@gmail.com>
2026-09-19 23:50:04 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace C48_G04_EXAM01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
